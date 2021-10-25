1a8c8b33ed930cc0f6e6b3ee95ed55a71bb9b2f7
Igor Pidik <igorPidik@gmail.com>
2021-10-25 19:10:40 +0200

    make connecting nicer

--- auth_token_frontend/src/App.js
+++ auth_token_frontend/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import {useWeb3React} from "@web3-react/core"
 import {injected} from "./connectors";
-import {useEffect} from "react";
+import React, {useEffect} from "react";
 
 
 function App() {
@@ -38,19 +38,28 @@ function App() {
     }
 
     return (
-
-        <div className="App">
-            <div className="flex flex-col items-center justify-center">
-                <button onClick={connect}
-                        className="py-2 mt-20 mb-4 text-lg font-bold text-white rounded-lg w-56 bg-blue-600 hover:bg-blue-800">Connect
-                    to MetaMask
-                </button>
-                {active ? <span>Connected with <b>{account}</b></span> : <span>Not connected</span>}
-                <button onClick={disconnect}
-                        className="py-2 mt-20 mb-4 text-lg font-bold text-white rounded-lg w-56 bg-blue-600 hover:bg-blue-800">Disconnect
-                </button>
-                <button onClick={generateSignedMessage}>Generate message</button>
+        <div className={'container-fluid m-2'}>
+            <div className={'row justify-content-md-center'}>
+                <div className={'col-auto col-offset'}>
+                    {active ?
+                        <div>
+                            <span>Connected with <b>{account}</b></span>
+                            <button onClick={disconnect}
+                                    className={'btn btn-danger'}>Disconnect
+                            </button>
+                        </div>
+                        :
+                        <button onClick={connect} className={'btn btn-primary'}>Connect to MetaMask</button>
+                    }
+                </div>
             </div>
+            {active &&
+            <div className={'row justify-content-md-center my-2'}>
+                <div className={'col-auto col-offset'}>
+                    <button className={'btn btn-primary'} onClick={generateSignedMessage}>Generate message</button>
+                </div>
+            </div>
+            }
         </div>
     );
 }
