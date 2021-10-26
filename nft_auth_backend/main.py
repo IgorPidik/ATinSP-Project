460c9ed28c09285d1f50219b3e72a220b7702a3d
Igor Pidik <igorPidik@gmail.com>
2021-10-26 22:14:11 +0200

    clean up backend code

--- nft_auth_backend/main.py
+++ nft_auth_backend/main.py
@@ -14,20 +14,28 @@ from token_required_wrapper import token_required
 
 app = Flask(__name__)
 CORS(app)
-w3 = Web3(Web3.HTTPProvider('http://127.0.0.1:8545'))
 
 
 @app.route('/restricted_service', methods=['GET'])
 @token_required
 def restricted_service():
+    """
+    restricted service (random number generator) which can be accessed only with a valid JWT token
+    """
     return jsonify({'random_number': random.randint(0, 9)})
 
 
 @app.route('/auth', methods=['POST'])
 def auth():
+    """
+    authentication method which first securely recovers signer address and uses smart contract to decide whether the
+    requester should be authenticated
+    """
     signed_message = request.json['signed_message']
     nft_id = request.json['nft_id']
+    # recover address of the signer
     signer = recover_signer(signed_message)
+    # use smart contract to check whether the signer is owner of the NFT and whether payment requirements were satisfied
     if authenticate(signer, nft_id):
         token = jwt.encode(
             # Make JWT valid till the midnight
@@ -40,19 +48,31 @@ def auth():
 
 
 def recover_signer(signed_message):
+    """
+    gets the address of the account that signed the message with their private key
+    """
     message_hash = defunct_hash_message(text='auth')
-    return w3.eth.account.recoverHash(message_hash, signature=signed_message)
+    return app.w3.eth.account.recoverHash(message_hash, signature=signed_message)
 
 
 def authenticate(account, token_id):
     now = datetime.datetime.utcnow()
+    # use smart contract to check whether the signer is owner of the NFT and whether payment requirements were satisfied
+    # for current month and year
     return app.auth_nft.functions.authenticate(account, token_id, now.year, now.month).call()
 
 
 if __name__ == '__main__':
     load_dotenv()
-    contract_json = json.load(open('compiled_contracts/AuthNFT.json', 'r'))
-    auth_nft = w3.eth.contract(address=os.environ.get('AUTH_TOKEN_CONTRACT_ADDRESS'), abi=contract_json['abi'])
+    # inject w3 instance
+    app.w3 = Web3(Web3.HTTPProvider('http://127.0.0.1:8545'))
 
+    # load contract
+    contract_json = json.load(open('compiled_contracts/AuthNFT.json', 'r'))
+    auth_nft = app.w3.eth.contract(address=os.environ.get('AUTH_TOKEN_CONTRACT_ADDRESS'), abi=contract_json['abi'])
+
+    # inject contract instance
     app.auth_nft = auth_nft
+
+    # run flask app
     app.run(debug=True)
